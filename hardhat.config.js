6609dd8efd22d20d36268b75c0455a1dc3b41760
Toshin Prince <toshinprince@gmail.com>
2023-08-14 10:20:33 +0530

    sepolia_update

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,18 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
 
 /** @type import('hardhat/config').HardhatUserConfig */
+
+const SEPOLIA_RPC_URL = process.env.ALCHEMY_SEPOLIA_RPC_URL;
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
+
 module.exports = {
   solidity: "0.8.17",
+  networks: {
+    sepolia: {
+      url: SEPOLIA_RPC_URL,
+      chainId: 11155111,
+      accounts: [PRIVATE_KEY],
+    },
+  },
 };
